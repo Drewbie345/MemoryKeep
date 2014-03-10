c9293cd782a86a8c64550a63bde09f4a9567a985
Drew Robinson <drewbie0307@hotmail.com>
2014-03-09 22:34:02 -0500

    Continuing to work on Views

---
+++ app/assets/javascripts/stories.js
@@ -0,0 +1,34 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+// You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).ready(function(){  
+  var reco = new WebSpeechRecognition();
+  reco.statusText('status');
+  reco.statusImage('img');
+  reco.finalResults('final_span');
+  reco.interimResults('interim_span');
+  reco.continuous = true;
+
+  reco.onEnd = function() {
+    reco.final_transcript;
+  };
+
+  $('#record').click(function(){
+    reco.toggleStartStop();
+    $("#body").val($("#results").text().trim())
+  })
+
+  $('#btn').click(function(){
+
+    if (reco.inProgress()) {
+      reco.stop();
+    } 
+    // var text = reco.final_transcript;
+
+    reco.onState('complete');
+    $('.speech').remove();
+
+  })
+
+});
