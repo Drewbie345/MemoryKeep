751c984e51477d677927afe26834d32edd680ef6
Brad Hubbard <brad.hubbard77@gmail.com>
2014-03-31 17:08:50 -0500

    Fix bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-git MemoryKeep::Application.configure do
+MemoryKeep::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
